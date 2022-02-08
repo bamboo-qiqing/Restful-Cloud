60cfdd611ea80734587e7ef989903910ffa862a8
guoqing <15225822319@163.com>
2022-02-08 23:25:59 +0800

    初次提交

--- src/main/java/com/bamboo/tool/db/SqlConstant.java
+++ src/main/java/com/bamboo/tool/db/SqlConstant.java
@@ -9,35 +9,35 @@ import com.bamboo.tool.util.StringUtil;
  */
 public class SqlConstant {
     /**
-     * ´´½¨apiÏîÄ¿±í
+     * åˆ›å»ºapié¡¹ç›®è¡¨
      */
     public static final String CREAT_PROJECT_SQL = "create table bamboo_api_project ( id integer not null constraint bamboo_api_project_pk primary key autoincrement, project_name text, project_path text, api_file_path text, api_file_name text );";
 
     /**
-     * ´´½¨api·½·¨±í
+     * åˆ›å»ºapiæ–¹æ³•è¡¨
      */
     public static final String CREAT_METHOD_SQL = "create table bamboo_api_method ( id integer not null constraint bamboo_api_method_pk primary key autoincrement, project_id integer not null, description text, method_name text, method_type text, content_type text, header text, params text, url text, model_name text, class_name text, class_desc text );";
 
     /**
-     * sqlite_master ²éÑ¯±í
+     * sqlite_master æŸ¥è¯¢è¡¨
      */
     public static final String SQLITE_MASTER_QUERY = "SELECT  * FROM sqlite_master WHERE type='table' AND name = '{}'";
 
     /**
-     * api ÏîÄ¿±íÃû
+     * api é¡¹ç›®è¡¨å
      */
     public static final String PROJECT_TABLE_NAME = "bamboo_api_project";
     /**
-     * api ·½·¨±íÃû
+     * api æ–¹æ³•è¡¨å
      */
     public static final String METHOD_TABLE_NAME = "bamboo_api_method";
 
     /**
-     * ²éÑ¯apiÏîÄ¿±íĞÅÏ¢
+     * æŸ¥è¯¢apié¡¹ç›®è¡¨ä¿¡æ¯
      */
     public static final String QUERY_PROJECT_TABLE = StringUtil.format(SQLITE_MASTER_QUERY, PROJECT_TABLE_NAME);
     /**
-     * ²éÑ¯api·½·¨±íĞÅÏ¢
+     * æŸ¥è¯¢apiæ–¹æ³•è¡¨ä¿¡æ¯
      */
     public static final String QUERY_METHOD_TABLE = StringUtil.format(SQLITE_MASTER_QUERY, METHOD_TABLE_NAME);
 }
