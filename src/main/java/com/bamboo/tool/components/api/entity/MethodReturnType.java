a2b2a962031640f913070e466a23bc6d428361dd
guoqing <15225822319@163.com>
2022-03-24 19:32:08 +0800

    初版

---
+++ src/main/java/com/bamboo/tool/components/api/entity/MethodReturnType.java
@@ -0,0 +1,23 @@
+package com.bamboo.tool.components.api.entity;
+
+import com.intellij.psi.PsiType;
+import com.intellij.psi.PsiTypeElement;
+import lombok.Data;
+
+import java.util.UUID;
+
+@Data
+public class MethodReturnType {
+    private String id;
+    private String returnType;
+    private String returnTypePath;
+
+    public void buildReturnType(PsiType psiType) {
+        this.returnType = psiType.getPresentableText();
+        this.returnTypePath = psiType.getCanonicalText();
+    }
+
+    public String toSql(String methodId) {
+        return "INSERT INTO bamboo_method_return_type (id, return_type, return_type_path, method_id) VALUES ('" + UUID.randomUUID() + "', '" + returnType + "', '" + returnTypePath + "', '" + methodId + "');";
+    }
+}
